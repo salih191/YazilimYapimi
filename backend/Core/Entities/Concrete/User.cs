fe6b487b78602bebc55e92aaeb0b365d402b2453
salih191 <58659931+salih191@users.noreply.github.com>
2021-04-21 00:33:11 +0300

    DataAccess and  Abstract folder added

---
+++ backend/Core/Entities/Concrete/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Entities.Concrete
+{
+    public class User : IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public string TCIdentityNumber { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}

74d4f0d0eb7ac399df39f1b6c0d2f03d1cbf64aa
salih191 <58659931+salih191@users.noreply.github.com>
2021-04-21 16:51:19 +0300

    Dependecy resolvers

--- backend/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ backend/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,7 +1,12 @@
 ﻿using Autofac;
 using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+using Business.Concrete;
 using Castle.DynamicProxy;
 using Core.Utilities.Interceptors;
+using Core.Utilities.Security.JWT;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
 
 namespace Business.DependencyResolvers.Autofac
 {
@@ -9,6 +14,33 @@ namespace Business.DependencyResolvers.Autofac
     {
         protected override void Load(ContainerBuilder builder)
         {
+            //user
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+            //auth
+            builder.RegisterType<AuthManager>().As<IAuthService>();
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>();
+            //addProduct
+            builder.RegisterType<AddProductManager>().As<IAddProductService>();
+            builder.RegisterType<EfAddProductDal>().As<IAddProductDal>();
+            //addMoney
+            builder.RegisterType<AddMoneyManager>().As<IAddMoneyService>();
+            builder.RegisterType<EfAddMoneyDal>().As<IAddMoneyDal>();
+            //orderDetail
+            builder.RegisterType<EfOrderDetailDal>().As<IOrderDetailDal>();
+            builder.RegisterType<OrderDetailManager>().As<IOrderDetailService>();
+            //order
+            builder.RegisterType<OrderManager>().As<IOrderService>();
+            builder.RegisterType<EfOrderDal>().As<IOrderDal>();
+            //product
+            builder.RegisterType<ProductManager>().As<IProductService>();
+            builder.RegisterType<EfProductDal>().As<IProductDal>();
+            //wallet
+            builder.RegisterType<WalletManager>().As<IWalletService>();
+            builder.RegisterType<EfWalletDal>().As<IWalletDal>();
+            //category
+            builder.RegisterType<CategoryManager>().As<ICategoryService>();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>();
 
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
 
